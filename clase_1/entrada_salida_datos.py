1f5f088453d668d10304d5165d2d541241156b1f
Javier <jgaitez@mi.unc.edu.ar>
2022-09-04 15:57:20 -0300

    Renombrado de archivo Hola Mundo.py por uno que se adecue a la info que contiene.

---
+++ clase_1/entrada_salida_datos.py
@@ -0,0 +1,49 @@
+# Salida de Datos
+# Función PRINT
+# Parametros extras: sep, end
+
+import imp
+
+
+print(".-:Hola Mundo:-.\n")
+print("Desde Visual")
+print("Y con extensiones\nque te facilitan codear\n")
+
+# comentario comentando comentarios
+
+# Otras funciones del print
+# Ops aritmeticas 
+print(2+2)
+
+# \n salto de linea
+print("Acá estamos viendo \nun salto de línea")
+
+# \t tabulacion
+print("Y acá una \t tabulacion")
+
+# concatenar (con el signo más)
+# como en excel, la concatenación une todo lo que se especifique como valores
+# lo que significa que si no se concatenan espacios, todos los valores van
+# a quedar "pegados"
+print("Valor 1"+"Valor 2")
+
+# Para agregar espacios: se puede agregar un espacio al ultimo del primer valor
+# o al inicio del siguiente. Tambien se puede usar "sep", en esta ocasión, los
+# valores deben ir separados con coma. En MI CASO, sin usar "sep" los valores 
+# se separan con un espacio por default.
+print("Valor 1","Valor 2")
+
+# Aca usando otro "separador" distinto de la coma para mostrar el uso de "sep"
+print("Valor 1", "Valor 2", sep= "-")
+
+# Función END
+# la funcion print por default termina en un salto de linea para que la proxima
+# función empiece en la línea inferior. Con "End", podemos modificar ese 
+# comportamiento
+
+print("Esto es una prueba para entender", end="\t")
+print("Le entiendo o no?")
+
+# Entrada de datos
+# Funcion INPUT
+input("Mensaje para avisar al usuario que el sistema esta a la espera de\nun ingreso de info: ")
