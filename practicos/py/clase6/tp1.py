0442813b4f8ec2bb2e3fb48a14df197206f503f4
Javier <jgaitez@mi.unc.edu.ar>
2022-10-01 02:19:43 -0300

    Inicio Juego

---
+++ practicos/py/clase6/tp1.py
@@ -0,0 +1,27 @@
+# Transformar en clase la informacion vertida encontrada en
+# https://www.leagueoflegends.com/es-mx/champions
+
+
+class Campeon:
+    def __init__(self):
+        self.nombre = ""
+        self.alias = ""
+        self.nombre_real = ""
+        self.especie = ""
+        self.origen = ""
+        self.arma = ""
+        self.estado = ""
+        self.residencia = ""
+        self.ocupacion = ""
+        self.region = ""
+        self.rol = ""
+        self.dificultad = ""
+        self.hab_pasiva = ""
+        self.hab_Q = ""
+        self.hab_W = ""
+        self.hab_E = ""
+        self.hab_R = ""
+        self.aspectos = ""
+        self.alcance = ""
+        self.mana = ""
+
