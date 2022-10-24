508da88bee60a85cd8a86b519cf3349fb2381cf5
Javier <jgaitez@mi.unc.edu.ar>
2022-10-24 13:09:25 -0300

    Creación de la primer view

--- practicos/dj/clase_16/classicmodels/products/views.py
+++ practicos/dj/clase_16/classicmodels/products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# Rest Imports
+from rest_framework.views import APIView
+
+# Models imports
+from products.models import Product
+
+# Serializer imports
+
 
 # Create your views here.
+
+class ProductApiView(APIView):
+    
+    def get(self, request):
+        """Retorna el listado de productos almacenados
+
+        Args:
+            request (_type_): _description_
+        """
