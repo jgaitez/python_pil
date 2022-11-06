daa6b84236a11ee5dadc6c3b2b242d99c09c3ba7
Javier <jgaitez@mi.unc.edu.ar>
2022-11-06 19:07:15 -0300

    Actualizacion Teorico al 25/10

--- dj/heroesApp/heroe/urls.py
+++ dj/heroesApp/heroe/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 
 # Views import
-from heroe.views import HeroApiView
+from heroe.views import HeroApiView, CrearHeroApiView, HeroDetalleApiView
 
 # Urls
 urlpatterns = [
     path("heroe-list/", HeroApiView.as_view(), name="heroe-list"),
+    path("create-heroe/", CrearHeroApiView.as_view(), name="Create"),
+    path("detalle-heroe/<int:pk>/", HeroDetalleApiView.as_view(), name="Detalle"),
 ]
--- dj/heroesApp/heroe/views.py
+++ dj/heroesApp/heroe/views.py
@@ -23,3 +23,80 @@ class HeroApiView(APIView):
             data=heroes_serializer.data,
             status=status.HTTP_200_OK
         )
+    
+
+class CrearHeroApiView(APIView):
+
+    def post(self, request):
+        """Crear un nuevo registro/heroe"""
+        print("Segundo POST")
+        serializer = HeroSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+        
+            data = {
+                "message":"El heroe fue creado de forma correcta"
+            }
+            return Response(
+                data=data,
+                status = status.HTTP_201_CREATED
+            )
+    
+        return Response(
+        data = serializer.errors,
+        status = status.HTTP_400_BAD_REQUEST
+    )
+
+
+class HeroDetalleApiView(APIView):
+
+    def get(self, request, pk):
+        """Nos retorna mas info de un heroe en particular"""
+
+        heroe = Hero.objects.get(pk=pk)
+
+        heroe_serializer = HeroSerializer(heroe)
+
+        return Response(
+            data=heroe_serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def put(self, request, pk):
+        """Nos modifica un heroe en particular"""
+
+        heroe = Hero.objects.get(pk=pk)
+
+        heroe_serializer = HeroSerializer(heroe, data=request.data)
+
+        if heroe_serializer.is_valid():
+            heroe_serializer.save()
+        
+            data = {
+                "message":"El heroe fue modificado de forma correcta"
+            }
+
+            return Response(
+                data=data,
+                status=status.HTTP_200_OK
+            )
+        
+        return Response(
+            data = heroe_serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        """Elimina un heroe"""
+
+        heroe = Hero.objects.get(pk=pk)
+        heroe.delete()
+
+        data = {
+            "message":"El heroe fue eliminado de forma correcta"
+        }
+        return Response(
+            data=data,
+            status=status.HTTP_200_OK
+        )
